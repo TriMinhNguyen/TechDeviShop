f293360ede854eea923f64cd3fbba39859e4d6de
Nguyen Minh Tri <tringuyen19xx@hotmail.com>
2018-07-06 10:29:24 +0700

    Update

--- TechDeviShopVs002/Models/About.cs
+++ TechDeviShopVs002/Models/About.cs
@@ -1,4 +1,4 @@
-namespace TechDeviShopVs002.Models
+﻿namespace TechDeviShopVs002.Models
 {
     using System;
     using System.Collections.Generic;
@@ -11,19 +11,24 @@ namespace TechDeviShopVs002.Models
     {
         public int AboutID { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Bạn phải nhập tên thông tin")]
+        [Display(Name = "Tên thông tin")]
         [StringLength(250)]
         public string AboutName { get; set; }
 
+        [Display(Name = "Thẻ tiêu đề SEO")]
         [StringLength(250)]
         public string MetaTitle { get; set; }
 
+        [Display(Name = "Mô tả")]
         [StringLength(500)]
         public string Description { get; set; }
 
+        [Display(Name = "Link ảnh")]
         [StringLength(250)]
         public string Image { get; set; }
 
+        [Display(Name = "Chi tiết")]
         [StringLength(500)]
         public string Detail { get; set; }
 
@@ -33,14 +38,19 @@ namespace TechDeviShopVs002.Models
         [StringLength(250)]
         public string MetalDescriptions { get; set; }
 
+        [Display(Name = "Ngày khởi tạo")]
         public DateTime? CreateDate { get; set; }
 
+        [Display(Name = "Người khởi tạo")]
         public int? CreateUser { get; set; }
 
+        [Display(Name = "Ngày sửa đổi")]
         public DateTime? ModifiedDate { get; set; }
 
+        [Display(Name = "Người sửa đổi")]
         public int? ModifiedUser { get; set; }
 
+        [Display(Name = "Kích hoạt")]
         public bool IsActive { get; set; }
     }
 }
